ae326129d048532cca2801119907eed94ca200f5
Srinivas Reddy G <sreddy222@yahoo.com>
2015-05-15 19:11:29 +0530

    responsive code

---
+++ magento-hard-code-snippets.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Magento work Info</title>
+</head>
+
+<body>
+
+jquery drop down works 
+-------------------------------------------------------------
+http://www.marghoobsuleman.com/jquery-image-dropdown
+-------------------------------------------------------------
+
+<h2> pdp page path  and form Catolgue / view -folder/  media images will come  </h2> 
+app/design/frontend/default/blank/template/catalog/product/view.phtml 
+<!--in html for testing purpose -->
+<h1><?php echo $this->__('sriniva -reddy-testing test') ?></h1>
+</body>
+</html>
